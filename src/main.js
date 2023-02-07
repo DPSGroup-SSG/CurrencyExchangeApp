92df3ca516bf4e04678bc3834647b7cd02b6e781
Dominic Dolan <Dominic.Dolan@dpsgroupglobal.com>
2023-02-07 10:52:17 +0000

    Removed files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style/reset.css'
-import './style/style.css'
-import App from './App.vue'
-import {createPinia} from "pinia";
-
-createApp(App)
-    .use(createPinia())
-    .mount('#app')
